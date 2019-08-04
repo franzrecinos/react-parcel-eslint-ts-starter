1e2ac62e432c321c98cb290a9cda161eacc0dee5
Franz Recinos <flamana@gmail.com>
2019-08-04 10:15:05 -0400

    Chore: commented out unecessary config

--- config/release.config.js
+++ config/release.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   analyzeCommits: {
     preset: 'eslint',
-    releaseRules: './config/release-rules.js', // optional, only if you want to set up new/modified release rules inside another file
+    // releaseRules: './config/release-rules.js', // optional, only if you want to set up new/modified release rules inside another file
     parserOpts: {
       // optional, only you want to have emoji commit support
       headerPattern: /^(?::([\w-]*):)?\s*(\w*):\s*(.*)$/,
