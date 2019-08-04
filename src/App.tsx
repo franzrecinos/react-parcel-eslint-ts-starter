4dab4623a7c4b4a5d44388942867601e370cf2dd
Franz Recinos <flamana@gmail.com>
2019-08-04 10:05:43 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = (): React.ReactElement => {
+  return (
+    <p>
+      Hello
+      <span role='img' aria-label='hello world'>
+        🌍
+      </span>
+      !
+    </p>
+  );
+};
+
+export default App;
